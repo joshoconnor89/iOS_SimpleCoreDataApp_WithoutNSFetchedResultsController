4350bb64fbdcae8ec5103bf947433cda7b80fef3
Josh O'Connor <Josh@joshuas-macbook-air.local>
2016-04-15 21:36:46 -0700

    Added update person method.

--- coredata/ViewController.m
+++ coredata/ViewController.m
@@ -18,8 +18,8 @@
     [super viewDidLoad];
     
     [self insertNewPerson];
-    [self deletePerson];
-    
+    //[self deletePerson];
+    [self updatePerson];
 }
 
 
@@ -57,6 +57,47 @@
         for (NSManagedObject* toDelete in results) {
             [coreDataStackInstance.managedObjectContext deleteObject:toDelete];
             [coreDataStackInstance saveContext];
+        }
+    }
+}
+
+
+-(void)updatePerson{
+    
+    CoreDataStack *coreDataStackInstance = [CoreDataStack defaultStack];
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    [fetchRequest setEntity:[NSEntityDescription entityForName:@"Person" inManagedObjectContext:coreDataStackInstance.managedObjectContext]];
+    
+    
+    NSString* nameVal = @"Josh";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == %@", nameVal];
+    [fetchRequest setPredicate:predicate];
+    
+    //Updating With more than one parameter
+    //    NSString* contentVal = @"test";
+    //    NSNumber* pageVal = [NSNumber numberWithInt:5];
+    //    NSString* bookVal = @"1331313";
+    //    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"content == %@ AND page_id == %@ AND book_id == %@", contentVal, pageVal, bookVal]];
+    
+    NSError *error = nil;
+    NSArray *results = [coreDataStackInstance.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+
+    if(results && [results count]>0){
+        for (Person* toUpdate in results) {
+            NSLog(@"Before Updating...");
+            NSLog(@"Weight attribute: %@", toUpdate.weight);
+            NSLog(@"Height attribute: %@", toUpdate.height);
+            NSLog(@"Name attribute: %@", toUpdate.name);
+            
+            [toUpdate setValue:[NSNumber numberWithInt:188] forKey:@"weight"];
+            [toUpdate setValue:[NSNumber numberWithInt:66] forKey:@"height"];
+            [toUpdate setValue:@"joe" forKey:@"name"];
+            [coreDataStackInstance saveContext];
+            
+            NSLog(@"After Updating...");
+            NSLog(@"Weight attribute: %@", toUpdate.weight);
+            NSLog(@"Height attribute: %@", toUpdate.height);
+            NSLog(@"Name attribute: %@", toUpdate.name);
         }
     }
 }
